a448c9688f1a1bd8a2a78c36d42781cb23f0ebd7
DrMax <tehnolog3ooo@yandex.ru>
2022-08-09 20:39:33 +0300

    Commit_002

--- Assets/WalkToWay.cs
+++ Assets/WalkToWay.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WayPointMovement : MonoBehaviour
+public class WalkToWay : MonoBehaviour
 {
     [SerializeField] private Transform _path;
     [SerializeField] private float _speed;
