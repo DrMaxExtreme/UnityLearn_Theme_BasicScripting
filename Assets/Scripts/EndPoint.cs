90cbca11d5408a0a84ac03d4608ef806974d4e50
DrMax <tehnolog3ooo@yandex.ru>
2022-08-10 10:28:49 +0300

    EndPoint_Trigger

---
+++ Assets/Scripts/EndPoint.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private Color _reaghtColor;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<Player>(out Player player))
+        {
+            _renderer.color = _reaghtColor;
+        }
+    }
+}
